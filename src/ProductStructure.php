210d411d02e028bbf15d053460e1d24c2aec03af
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-02-12 08:30:38 -0300

    Primeira versão do teste de lógica

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,107 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $array_old = self::products;
+        $final_array = [];
+
+
+        foreach($array_old as $chave => $array)
+        {
+            $key = explode('-', $array);
+
+            if(strpos($array, 'preto-') !== false)
+            {
+                if($key[1] === 'PP')
+                {
+                    $final_array['preto'][$key[1]] += 1;
+                }
+                else if($key[1] === 'M')
+                {
+                    $final_array['preto'][$key[1]] += 1;
+
+                }
+                else if($key[1] === 'G')
+                {
+                    $final_array['preto'][$key[1]] += 1;
+
+                }
+                else
+                {
+                    $final_array['preto'][$key[1]] += 1;
+
+                }
+            }
+
+            if(strpos($array, 'branco-') !== false)
+            {
+                if($key[1] === 'PP')
+                {
+                    $final_array['branco'][$key[1]] += 1;
+                }
+                else if($key[1] === 'M')
+                {
+                    $final_array['branco'][$key[1]] += 1;
+
+                }
+                else if($key[1] === 'G')
+                {
+                    $final_array['branco'][$key[1]] += 1;
+
+                }
+                else
+                {
+                    $final_array['branco'][$key[1]] += 1;
+
+                }
+            }
+
+            if(strpos($array, 'vermelho-') !== false)
+            {
+                if($key[1] === 'PP')
+                {
+                    $final_array['vermelho'][$key[1]] += 1;
+                }
+                else if($key[1] === 'M')
+                {
+                    $final_array['vermelho'][$key[1]] += 1;
+
+                }
+                else if($key[1] === 'G')
+                {
+                    $final_array['vermelho'][$key[1]] += 1;
+
+                }
+                else
+                {
+                    $final_array['vermelho'][$key[1]] += 1;
+
+                }
+            }
+
+            if(strpos($array, 'azul-') !== false)
+            {
+                if($key[1] === 'PP')
+                {
+                    $final_array['azul'][$key[1]] += 1;
+                }
+                else if($key[1] === 'M')
+                {
+                    $final_array['azul'][$key[1]] += 1;
+
+                }
+                else if($key[1] === 'G')
+                {
+                    $final_array['azul'][$key[1]] += 1;
+
+                }
+                else
+                {
+                    $final_array['azul'][$key[1]] += 1;
+
+                }
+            }
+        }
+
+        return $final_array;
     }
 }
