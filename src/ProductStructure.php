aa58ec4b68c26791585a9bd744fb9cb51f491c07
Daniel Carvalho <danielcarvalhodev@gmail.com>
2022-02-12 08:53:18 -0300

    Otimização/Remoção de código repetitivo

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -18,96 +18,41 @@ class ProductStructure
         "azul-P"
     ];
 
+    public $final_array = [];
+
+    public function mount ($array, $key)
+    {
+        if(strpos($array, $key[0].'-') !== false)
+        {
+            if($key[1] === 'PP')
+            {
+                $this->final_array[$key[0]][$key[1]] += 1;
+            }
+            else if($key[1] === 'M')
+            {
+                $this->final_array[$key[0]][$key[1]] += 1;
+            }
+            else if($key[1] === 'G')
+            {
+                $this->final_array[$key[0]][$key[1]] += 1;
+            }
+            else
+            {
+                $this->final_array[$key[0]][$key[1]] += 1;
+            }
+        }
+    }
+
     public function make(): array
     {
         $array_old = self::products;
-        $final_array = [];
-
+        
         foreach($array_old as $array)
         {
             $key = explode('-', $array);
-
-            if(strpos($array, 'preto-') !== false)
-            {
-                if($key[1] === 'PP')
-                {
-                    $final_array['preto'][$key[1]] += 1;
-                }
-                else if($key[1] === 'M')
-                {
-                    $final_array['preto'][$key[1]] += 1;
-                }
-                else if($key[1] === 'G')
-                {
-                    $final_array['preto'][$key[1]] += 1;
-                }
-                else
-                {
-                    $final_array['preto'][$key[1]] += 1;
-                }
-            }
-
-            if(strpos($array, 'branco-') !== false)
-            {
-                if($key[1] === 'PP')
-                {
-                    $final_array['branco'][$key[1]] += 1;
-                }
-                else if($key[1] === 'M')
-                {
-                    $final_array['branco'][$key[1]] += 1;
-                }
-                else if($key[1] === 'G')
-                {
-                    $final_array['branco'][$key[1]] += 1;
-                }
-                else
-                {
-                    $final_array['branco'][$key[1]] += 1;
-                }
-            }
-
-            if(strpos($array, 'vermelho-') !== false)
-            {
-                if($key[1] === 'PP')
-                {
-                    $final_array['vermelho'][$key[1]] += 1;
-                }
-                else if($key[1] === 'M')
-                {
-                    $final_array['vermelho'][$key[1]] += 1;
-                }
-                else if($key[1] === 'G')
-                {
-                    $final_array['vermelho'][$key[1]] += 1;
-                }
-                else
-                {
-                    $final_array['vermelho'][$key[1]] += 1;
-                }
-            }
-
-            if(strpos($array, 'azul-') !== false)
-            {
-                if($key[1] === 'PP')
-                {
-                    $final_array['azul'][$key[1]] += 1;
-                }
-                else if($key[1] === 'M')
-                {
-                    $final_array['azul'][$key[1]] += 1;
-                }
-                else if($key[1] === 'G')
-                {
-                    $final_array['azul'][$key[1]] += 1;
-                }
-                else
-                {
-                    $final_array['azul'][$key[1]] += 1;
-                }
-            }
+            $this->mount($array, $key);
         }
 
-        return $final_array;
+        return $this->final_array;
     }
 }
